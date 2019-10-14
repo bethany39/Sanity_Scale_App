49211464403e0cabc114e4b968e1949c870d622d
Bethany Ebbitt <bebbitt@live.unc.edu>
2019-10-14 00:10:25 -0400

    Add sign up screen designs, but did not link up the buttons yet.

---
+++ app/src/main/java/com/example/sanityscaleapp/IWantTo.java
@@ -0,0 +1,14 @@
+package com.example.sanityscaleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IWantTo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iwant_to);
+    }
+}
---
+++ app/src/main/java/com/example/sanityscaleapp/PreferredUnits.java
@@ -0,0 +1,14 @@
+package com.example.sanityscaleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreferredUnits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferred_units);
+    }
+}
