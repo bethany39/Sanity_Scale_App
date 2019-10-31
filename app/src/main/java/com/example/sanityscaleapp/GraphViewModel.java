4cf589d0f1b381eee4891b508c5215425323b4ea
Bethany Ebbitt <bebbitt@live.unc.edu>
2019-10-31 16:04:41 -0400

    Not working yet, pushing to save code and going to pull from master and start from scratch...

--- app/src/main/java/com/example/sanityscaleapp/GraphViewModel.java
+++ app/src/main/java/com/example/sanityscaleapp/GraphViewModel.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GraphViewModel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_graph_view_model);
     }
 }
